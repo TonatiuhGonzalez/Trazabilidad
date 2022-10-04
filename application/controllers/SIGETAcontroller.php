842704b9bfd92d3989b7c8c14b2d93275213b9d3
Tonatiuh <gonz.tonatiuh@gmail.com>
2022-10-03 19:48:05 -0500

    Se eliminaron comentarios de SIGETAcontroller

--- application/controllers/SIGETAcontroller.php
+++ application/controllers/SIGETAcontroller.php
@@ -8,11 +8,6 @@ class SIGETAcontroller extends CI_Controller {
 	}
 	public function index(){
 		if($this->session->userdata('logged_in')){
-
-			//$data=array(
-			//	'establecimientos'=>$this->establecimiento_model->get_establecimientos()
-			//);
-			//echo json_encode($resultado);
             $this->load->view('admin/template/barra-nav');
 			$this->load->view('admin/template/columna');	
 			$this->load->view('admin/template/footer');

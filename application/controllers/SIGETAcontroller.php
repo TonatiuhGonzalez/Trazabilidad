75810e6bc17d47e93f27896be48fd98338e5573c
Tonatiuh <gonz.tonatiuh@gmail.com>
2022-10-03 19:55:01 -0500

    Se borró un comentario en sigeta controller

--- application/controllers/SIGETAcontroller.php
+++ application/controllers/SIGETAcontroller.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SIGETAcontroller extends CI_Controller {
 	public function __construct(){
-		parent::__construct();
-		//$this->load->model('establecimiento_model');		
+		parent::__construct();	
 	}
 	public function index(){
 		if($this->session->userdata('logged_in')){
